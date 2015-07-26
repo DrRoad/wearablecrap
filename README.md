d214903c6218c2e9e5410e0db7ae7278a9ebdb3e
phardwick <phillip@creativeliveagency.com>
2015-07-26 16:01:23 -0500

    Changes to be committed: 	new file:   CodeBook.rmd 	modified:   README.md 	modified:   run_analysis.R

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# wearablecrap
-Tidy data set of data on IoT invaders that you can wear
+---
+title: "ReadMe"
+author: "YoursTruly"
+date: "July 26, 2015"
+output: html_document
+---
+One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Here are the data for the project:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+ You should create one R script called run_analysis.R that does the following. 
+
+    Merges the training and the test sets to create one data set.
+    Extracts only the measurements on the mean and standard deviation for each measurement. 
+    Uses descriptive activity names to name the activities in the data set
+    Appropriately labels the data set with descriptive variable names. 
+
+    From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+Good luck!
+
+There is one Script called run_analysis.R that will import and process all data as long as the raw data is in your working directory in the same structure it was provided in. CodeBook.rmd explains the process and all variables. Tidysetofmeansbyactbysub.txt is a csv of the results of run_analysis.R, should you be unable to run the script to verify. Roses are red, violets are indigo, get your eyes checked.
--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,35 @@
-setwd("~/coursera/gettingdata")
+setwd("~/coursera/wearablecrap")  ## change to reflect your local workspace. data MUST be in your workspace and in in the original structure it was made available in for the script to work without additional changes
 library(dplyr)
 library(data.table)
-make_list <- function(labels){
+make_list <- function(labels){  ## function for making labels to subset with later
   as.vector(mean_std <- c(1,2,grep(".*-mean\\(.*|.*-std\\(.*",labels, value = FALSE)))
   names(mean_std) <- c("activity","subject",grep(".*-mean\\(.*|.*-std\\(.*",labels, value = TRUE))
   return(mean_std)
 }
 
-labels <- as.list(c("activity","subject",as.vector(read.table("~/coursera/gettingdata/UCI HAR Dataset/features.txt")[,"V2"])))
-act_list <- as.list(c(as.vector(read.table("~/coursera/gettingdata/UCI HAR Dataset/activity_labels.txt")[,"V2"])))
-sub_act_df <- data.frame(as.list(read.table("~/coursera/gettingdata/UCI HAR Dataset/test/y_test.txt")),as.list(read.table("~/coursera/gettingdata/UCI HAR Dataset/test/subject_test.txt")))
-testbound <- dplyr::tbl_df(bind_cols(sub_act_df, read.table("~/coursera/gettingdata/UCI HAR Dataset/test/X_test.txt")))
-names(testbound) <- 1:length(testbound)
-sub_act_df_train <- data.frame(as.list(read.table("~/coursera/gettingdata/UCI HAR Dataset/train/y_train.txt")),as.list(read.table("~/coursera/gettingdata/UCI HAR Dataset/train/subject_train.txt")))
-trainbound <- dplyr::tbl_df(bind_cols(sub_act_df_train, read.table("~/coursera/gettingdata/UCI HAR Dataset/train/X_train.txt")))
-names(trainbound) <- 1:length(trainbound)
-test_train_all <- dplyr::union(testbound,trainbound)
-mean_std <- make_list(labels)
-mean_std_slect <- dplyr::select(test_train_all,mean_std) 
-mss_df = data.table(mean_std_slect)
-mean_set = data.table()
-
+labels <- c("activity","subject",grep(".*-mean\\(.*|.*-std\\(.*",as.vector(read.table("features.txt")[,"V2"]), value = TRUE)) ##list of all labels
+act_list <- as.list(c(as.vector(read.table("activity_labels.txt")[,"V2"])))  ## list of activities performed during measurements
+testbound <- dplyr::tbl_df(bind_cols(data.frame(as.list(read.table("./test/y_test.txt")),as.list(read.table("./test/subject_test.txt"))), read.table("./test/X_test.txt"))) ## coallation of X Y Z data into single set
+names(testbound) <- 1:length(testbound) ## numbering columns for later bind
+trainbound <- dplyr::tbl_df(bind_cols(data.frame(as.list(read.table("./train/y_train.txt")),as.list(read.table("./train/subject_train.txt"))), read.table("./train/X_train.txt"))) ## coallation of X Y Z data into single set
+names(trainbound) <- 1:length(trainbound)  ## numbering columns for later bind
+mss_df <- data.table(dplyr::select(dplyr::union(testbound,trainbound),make_list(labels)))  ## binding train and test datasets into single data.table
+rm(testbound)
+rm(trainbound)
+mean_set = data.table() ## empty table for summary data
 for(i in 1:30){
   tmp <- data.table()
   for(h in 1:6){
-    tmp <- dplyr::summarise_each(mss_df[which((mss_df$activity == h) & (mss_df$subject == i))],funs(mean))
-    tmp[, activity := as.character(activity)][activity == h, activity := act_list[[h]]]
-    mean_set <- dplyr::bind_rows(mean_set,tmp)
+    tmp <- dplyr::summarise_each(mss_df[which((mss_df$activity == h) & (mss_df$subject == i))],funs(mean))  ## creation of summary data
+    tmp[, activity := as.character(activity)][activity == h, activity := act_list[[h]]]  ##changing activity 'codes' into activity "names" for tidy set
+    mean_set <- dplyr::bind_rows(mean_set,tmp)  ##combining summary data into tidy data set
   }
 }
+for(h in 1:6){
+  mss_df[, activity := as.character(activity)][activity == h, activity := act_list[[h]]] ##changing activity 'codes' into activity "names" for wide set
+}
+rm(tmp)  ## mopping the floors
+rm(act_list)
+rm(labels)
+rm(h)
+rm(i)
